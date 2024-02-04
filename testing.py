33e40dd275ea63306bf1e0e58c5439792c76e87f
Dustin Jeiondre Ventura <100325624+DustinVentura@users.noreply.github.com>
2024-02-04 18:04:01 +0800

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-from API_Access import getPrediction
-
-image_path = 'C:/Users/Cliff/Documents/School Files/MAPUA/2023 - 2024 Term 2/CPE124/Model/From Group 2/Testing/testingForAPI.jpg'
-data=getPrediction(image_path)
-classification = data.get('class')
-prediction = round(float(data.get('confidence')),5)
-confidence = str(prediction * 100) + '%'
-print(classification, confidence)
